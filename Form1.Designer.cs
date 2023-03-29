5b6740eddf42d7b377a995b7de9534d9d4481bdb
jaume <jmuste3@vidalibarraquer.net>
2023-03-29 08:42:24 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace DemoCustomControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttondam3 = new DemoCustomControl.ButtonDAM();
+            this.buttondam2 = new DemoCustomControl.ButtonDAM();
+            this.buttondam1 = new DemoCustomControl.ButtonDAM();
+            this.buttondam4 = new DemoCustomControl.ButtonDAM();
+            this.SuspendLayout();
+            // 
+            // buttondam3
+            // 
+            this.buttondam3.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.buttondam3.BackColor = System.Drawing.Color.Transparent;
+            this.buttondam3.BorderColor = System.Drawing.Color.Transparent;
+            this.buttondam3.BorderWidth = 2;
+            this.buttondam3.ButtonShape = DemoCustomControl.ButtonDAM.ButtonsShapes.Rect;
+            this.buttondam3.ButtonText = "ButtonDAM";
+            this.buttondam3.EndColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttondam3.FlatAppearance.BorderSize = 0;
+            this.buttondam3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttondam3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttondam3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttondam3.GradientAngle = 110;
+            this.buttondam3.Location = new System.Drawing.Point(493, 37);
+            this.buttondam3.MouseClickColor1 = System.Drawing.Color.Yellow;
+            this.buttondam3.MouseClickColor2 = System.Drawing.Color.Red;
+            this.buttondam3.MouseHoverColor1 = System.Drawing.Color.Turquoise;
+            this.buttondam3.MouseHoverColor2 = System.Drawing.Color.MediumSeaGreen;
+            this.buttondam3.Name = "buttondam3";
+            this.buttondam3.ShowButtontext = true;
+            this.buttondam3.Size = new System.Drawing.Size(187, 87);
+            this.buttondam3.StartColor = System.Drawing.Color.DodgerBlue;
+            this.buttondam3.TabIndex = 2;
+            this.buttondam3.Text = "buttondam3";
+            this.buttondam3.TextLocation_X = 10;
+            this.buttondam3.TextLocation_Y = 10;
+            this.buttondam3.Transparent1 = 250;
+            this.buttondam3.Transparent2 = 250;
+            this.buttondam3.UseVisualStyleBackColor = false;
+            // 
+            // buttondam2
+            // 
+            this.buttondam2.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.buttondam2.BackColor = System.Drawing.Color.Transparent;
+            this.buttondam2.BorderColor = System.Drawing.Color.Transparent;
+            this.buttondam2.BorderWidth = 2;
+            this.buttondam2.ButtonShape = DemoCustomControl.ButtonDAM.ButtonsShapes.Circle;
+            this.buttondam2.ButtonText = "ButtonDAM";
+            this.buttondam2.EndColor = System.Drawing.Color.MidnightBlue;
+            this.buttondam2.FlatAppearance.BorderSize = 0;
+            this.buttondam2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttondam2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttondam2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttondam2.GradientAngle = 90;
+            this.buttondam2.Location = new System.Drawing.Point(267, 215);
+            this.buttondam2.MouseClickColor1 = System.Drawing.Color.Yellow;
+            this.buttondam2.MouseClickColor2 = System.Drawing.Color.Red;
+            this.buttondam2.MouseHoverColor1 = System.Drawing.Color.Turquoise;
+            this.buttondam2.MouseHoverColor2 = System.Drawing.Color.DarkSlateGray;
+            this.buttondam2.Name = "buttondam2";
+            this.buttondam2.ShowButtontext = true;
+            this.buttondam2.Size = new System.Drawing.Size(198, 114);
+            this.buttondam2.StartColor = System.Drawing.Color.DodgerBlue;
+            this.buttondam2.TabIndex = 4;
+            this.buttondam2.Text = "buttondam2";
+            this.buttondam2.TextLocation_X = 65;
+            this.buttondam2.TextLocation_Y = 43;
+            this.buttondam2.Transparent1 = 250;
+            this.buttondam2.Transparent2 = 250;
+            this.buttondam2.UseVisualStyleBackColor = false;
+            // 
+            // buttondam1
+            // 
+            this.buttondam1.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.buttondam1.BackColor = System.Drawing.Color.Transparent;
+            this.buttondam1.BorderColor = System.Drawing.Color.Transparent;
+            this.buttondam1.BorderWidth = 2;
+            this.buttondam1.ButtonShape = DemoCustomControl.ButtonDAM.ButtonsShapes.RoundRect;
+            this.buttondam1.ButtonText = "ButtonDAM";
+            this.buttondam1.EndColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttondam1.FlatAppearance.BorderSize = 0;
+            this.buttondam1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttondam1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttondam1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttondam1.GradientAngle = 90;
+            this.buttondam1.Location = new System.Drawing.Point(49, 25);
+            this.buttondam1.MouseClickColor1 = System.Drawing.Color.Yellow;
+            this.buttondam1.MouseClickColor2 = System.Drawing.Color.Red;
+            this.buttondam1.MouseHoverColor1 = System.Drawing.Color.Turquoise;
+            this.buttondam1.MouseHoverColor2 = System.Drawing.Color.DarkSlateGray;
+            this.buttondam1.Name = "buttondam1";
+            this.buttondam1.ShowButtontext = false;
+            this.buttondam1.Size = new System.Drawing.Size(171, 99);
+            this.buttondam1.StartColor = System.Drawing.Color.CadetBlue;
+            this.buttondam1.TabIndex = 5;
+            this.buttondam1.Text = "buttondam1";
+            this.buttondam1.TextLocation_X = 56;
+            this.buttondam1.TextLocation_Y = 38;
+            this.buttondam1.Transparent1 = 250;
+            this.buttondam1.Transparent2 = 250;
+            this.buttondam1.UseVisualStyleBackColor = false;
+            // 
+            // buttondam4
+            // 
+            this.buttondam4.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.buttondam4.BackColor = System.Drawing.Color.Transparent;
+            this.buttondam4.BorderColor = System.Drawing.Color.Transparent;
+            this.buttondam4.BorderWidth = 2;
+            this.buttondam4.ButtonShape = DemoCustomControl.ButtonDAM.ButtonsShapes.Rect;
+            this.buttondam4.ButtonText = "ButtonDAM";
+            this.buttondam4.EndColor = System.Drawing.Color.MidnightBlue;
+            this.buttondam4.FlatAppearance.BorderSize = 0;
+            this.buttondam4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttondam4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttondam4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttondam4.GradientAngle = 90;
+            this.buttondam4.Location = new System.Drawing.Point(153, 203);
+            this.buttondam4.MouseClickColor1 = System.Drawing.Color.Yellow;
+            this.buttondam4.MouseClickColor2 = System.Drawing.Color.Red;
+            this.buttondam4.MouseHoverColor1 = System.Drawing.Color.Turquoise;
+            this.buttondam4.MouseHoverColor2 = System.Drawing.Color.DarkSlateGray;
+            this.buttondam4.Name = "buttondam4";
+            this.buttondam4.ShowButtontext = true;
+            this.buttondam4.Size = new System.Drawing.Size(100, 40);
+            this.buttondam4.StartColor = System.Drawing.Color.DodgerBlue;
+            this.buttondam4.TabIndex = 6;
+            this.buttondam4.Text = "buttondam4";
+            this.buttondam4.TextLocation_X = 32;
+            this.buttondam4.TextLocation_Y = 18;
+            this.buttondam4.Transparent1 = 250;
+            this.buttondam4.Transparent2 = 250;
+            this.buttondam4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttondam4);
+            this.Controls.Add(this.buttondam1);
+            this.Controls.Add(this.buttondam2);
+            this.Controls.Add(this.buttondam3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ButtonDAM buttondam3;
+        private ButtonDAM buttondam2;
+        private ButtonDAM buttondam1;
+        private ButtonDAM buttondam4;
+    }
+}
